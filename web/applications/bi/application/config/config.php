47b5770ee8119ad1b99a1709d208163684079992
WangZejun <18310348218@163.com>
2015-07-09 12:01:17 +0800

    update bi/app/config/config.base_url

--- web/applications/bi/application/config/config.php
+++ web/applications/bi/application/config/config.php
@@ -14,10 +14,10 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://bi.firstgit.net';
+$config['base_url']	= 'http://bi.dachuwang.com';
 $config['img_url']	= 'http://img.ymt360.com/';
 $config['api_url']  = 'http://api.dachuwang.com/';
-$config['web_url']  = 'http://bi.dachuwang.com/';
+$config['web_url']  = 'http://bi.firstgit.net/';
 $config['location_url']   = 'http://10.144.243.167:10020/?q=';
 /*
 |--------------------------------------------------------------------------
