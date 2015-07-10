cf0972b668ffe6b82c4aa2d81d0890977794f646
WangZejun <18310348218@163.com>
2015-07-10 18:20:25 +0800

    hop config

---
+++ web/applications/bi/application/config/development/service.php
@@ -0,0 +1,8 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+// 服务域名
+$config = array(
+    // 对内service域名
+    's' => 's.dachuwang.net',
+    // 对外系统service域名
+    'api' => 'api.dachuwang.com'
+);
