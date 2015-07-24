95980c36d85ab2a0fefce5baaf4096cb218946df
WangZejun <18310348218@163.com>
2015-07-24 18:02:03 +0800

    s

---
+++ index.php
@@ -0,0 +1,1 @@
+<html>  <head>    <title>first bootstrap example</title>  </head>  <body>    <form>      <div class="checkbox">        <label>          <input type="checkbox" />it          <input type="checkbox" />he          <input type="checkbox" />she        </label>      </div>    </form>  </body></html>
